05a311ca74b0b10d00ed4a538b3b824e57796c63
shashi-web <raoshashidhar.3@gmail.com>
2025-01-22 19:55:49 -0500

    AWS provider configuration with variables

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  description = "aws region to deploy the resources"
+  default = "us-east-1"
+}
+
+variable "access_key" {
+  description = "your aws access key"
+  sensitive = true
+  type = string
+}
+
+variable "secret_key" {
+  description = "your aws secret key"
+  sensitive = true
+  type = string
+}
