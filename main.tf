8c09da598ec288ddc11c50297e0dcabaabcd0228
shashi-web <raoshashidhar.3@gmail.com>
2025-01-22 20:32:31 -0500

    created the private and the public subnets across availability zones

--- main.tf
+++ main.tf
@@ -21,3 +21,30 @@ resource "aws_vpc" "project_vpc" {
     Name = "proj-vpc"
   }
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public_subnet" {
+  count = 3
+  vpc_id     = aws_vpc.project_vpc.id
+  cidr_block = cidrsubnet(var.vpc_cidr_block, 3, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count = 3
+  vpc_id     = aws_vpc.project_vpc.id
+  cidr_block = cidrsubnet(var.vpc_cidr_block, 3, count.index+3)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+
+  tags = {
+    Name = "private-subnet-${count.index+1}"
+  }
+}
