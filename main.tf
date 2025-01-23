917cf19ff854f1116566667863623e710dc68ac1
shashi-web <raoshashidhar.3@gmail.com>
2025-01-22 20:44:42 -0500

    configured Internet Gateway and public subnet routing

--- main.tf
+++ main.tf
@@ -48,3 +48,30 @@ resource "aws_subnet" "private_subnet" {
     Name = "private-subnet-${count.index+1}"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.project_vpc.id
+
+  tags = {
+    Name = "proj-igw"
+  }
+}
+
+resource "aws_route_table" "public_rtb" {
+  vpc_id = aws_vpc.project_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "proj_public_rtb"
+  }
+}
+
+resource "aws_route_table_association" "public_rtb_association" {
+  count = 3
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rtb.id
+}
